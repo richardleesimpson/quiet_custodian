91fef36ec76a5f4bdbb76aeefb43c439049f1e40
richardleesimpson <github.elag9@passmail.net>
2024-03-13 13:29:56 -0500

    Take source dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{env, path::PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <source_folder>", args[0]);
+        std::process::exit(1);
+    }
+    
+    let source_folder = PathBuf::from(&args[1]);
+
+    println!("{source_folder:?}");
 }
