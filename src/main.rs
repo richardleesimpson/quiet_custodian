5426c2aa32725147d8e22ccc0545a5298d78bdb2
richardleesimpson <github.elag9@passmail.net>
2024-03-13 14:27:22 -0500

    Add notify and watch folder

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{env, path::PathBuf};
+use std::{ env, path::{ Path, PathBuf } };
+use notify::{ Config, RecommendedWatcher, RecursiveMode, Watcher };
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,8 +7,27 @@ fn main() {
         eprintln!("Usage: {} <source_folder>", args[0]);
         std::process::exit(1);
     }
-    
+
     let source_folder = PathBuf::from(&args[1]);
 
-    println!("{source_folder:?}");
+    println!("The custodian is quietly listening to {source_folder:?} ...");
+
+    if let Err(error) = watch(source_folder) {
+        eprintln!("Error: {error:?}");
+    }
+}
+
+fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
+    let (tx, rx) = std::sync::mpsc::channel();
+    let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
+    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
+
+    for res in rx {
+        match res {
+            Ok(event) => println!("Change: {event:?}"),
+            Err(error) => eprintln!("Error: {error:?}"),
+        }
+    }
+
+    Ok(())
 }
