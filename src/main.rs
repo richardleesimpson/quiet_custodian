33db70b6974248db47dcb2e5b9fd9ee8f398e8f4
richardleesimpson <github.elag9@passmail.net>
2024-03-19 14:11:53 -0500

    Define input and output file name formats

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-use std::{ env, fs::{ rename }, path::PathBuf };
+#[macro_use]
+extern crate lazy_static;
 
+const INPUT_DATE_FORMAT: &str = "%y%m%d%H%M";
+const OUTPUT_DATE_FORMAT: &str = "%Y-%m-%d %H-%M";
+
+use std::{ env, fs::rename, path::PathBuf };
+use chrono::NaiveDateTime;
 use notify::{ Config, RecommendedWatcher, RecursiveMode, Watcher };
+use regex::Regex;
+
+lazy_static! {
+    static ref FILENAME_PATTERN: Regex = Regex::new(
+        r"R-\d{5}_(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})_REC(?i)\.mp3$"
+    ).unwrap();
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -29,9 +42,14 @@ fn watch(source: PathBuf, destination: PathBuf) -> notify::Result<()> {
             Ok(event) => {
                 println!("Change: {event:?}");
                 for path in event.paths {
-                    if path.exists() {
-                        let to = destination.join(path.file_name().unwrap());
+                    let file_name = path.file_name().unwrap().to_str().unwrap();
+                    let is_match = FILENAME_PATTERN.is_match(file_name);
+                    if path.exists() && is_match {
+                        let to = destination.join(reformat_filename(file_name));
                         println!("Moving {path:?} to {to:?}");
+                        if let Err(error) = std::fs::create_dir_all(&destination) {
+                            eprintln!("Error: {error:?}");
+                        }
                         rename(path, to).unwrap();
                     }
                 }
@@ -41,4 +59,20 @@ fn watch(source: PathBuf, destination: PathBuf) -> notify::Result<()> {
     }
 
     Ok(())
+}
+
+fn reformat_filename(filename: &str) -> String {
+    println!("Reformatting filename: {filename}");
+    if let Some(caps) = FILENAME_PATTERN.captures(filename) {
+        let date_str = format!("{}{}{}{}{}", &caps[1], &caps[2], &caps[3], &caps[4], &caps[5]);
+
+        if let Ok(date_time) = NaiveDateTime::parse_from_str(&date_str, INPUT_DATE_FORMAT) {
+            format!("{}.mp3", date_time.format(OUTPUT_DATE_FORMAT))
+        } else {
+            eprintln!("Could not parse date from filename: {}", filename);
+            filename.to_string()
+        }
+    } else {
+        filename.to_string()
+    }
 }
