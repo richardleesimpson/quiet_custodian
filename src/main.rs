8987e200008b698a7b2cadbc0484a48d27926cdb
richardleesimpson <github.elag9@passmail.net>
2024-04-14 21:55:27 -0500

    Better logging and better timestamps

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::{ ffi::OsStr, fs::{ metadata, rename }, path::{ Path, PathBuf } };
-use chrono::{ DateTime, Utc };
+use std::{ ffi::OsStr, fs::{ metadata, rename }, path::{ Path, PathBuf }, time::SystemTime };
+use chrono::{ DateTime, Local, Utc };
+use env_logger::Builder;
 use notify::{ Config, RecommendedWatcher, RecursiveMode, Watcher };
 use clap::Parser;
+use log::{ debug, error, info, LevelFilter };
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -15,24 +17,33 @@ struct Cli {
         long,
         value_name = "OUTPUT_FORMAT",
         help = "Output format",
-        default_value = "%Y-%m-%d %H-%M"
+        default_value = "%Y%m%d%H%M%S"
     )]
     output_format: String, // TODO: Rename this
+    #[arg(
+        short,
+        long,
+        help = "The logging level (error, warn, info, debug, trace)",
+        default_value = "info"
+    )]
+    log_level: LevelFilter,
+    // TODO: Make rename optional
 }
 
 fn main() {
     let cli: Cli = Cli::parse();
+    Builder::from_default_env().filter(None, cli.log_level).init();
 
-    println!("The custodian is quietly listening to {0:?} ...", cli.input);
+    info!("The custodian is quietly listening to {0:?} ...", cli.input);
 
     if let Err(error) = listen(cli.input, cli.output, &cli.output_format) {
-        eprintln!("Error listening: {error:?}");
+        error!("Error listening: {error:?}");
     }
 }
 
 fn listen(input: PathBuf, output: PathBuf, output_format: &str) -> notify::Result<()> {
     if let Err(error) = std::fs::create_dir_all(&input) {
-        eprintln!("Error creating source directory: {error:?}");
+        error!("Error creating source directory: {error:?}");
     }
 
     let (tx, rx) = std::sync::mpsc::channel();
@@ -42,25 +53,25 @@ fn listen(input: PathBuf, output: PathBuf, output_format: &str) -> notify::Resul
     for res in rx {
         match res {
             Ok(event) => {
-                println!("Change: {event:?}");
+                debug!("Change: {event:?}");
                 for path in event.paths {
                     if path.exists() && is_supported_audio_format(path.as_path()) {
                         let to = output.join(reformat_filename(&path, output_format));
                         if let Err(error) = transcribe_audio_file(&path, &to) {
-                            eprintln!("Error transcribing audio: {error:?}");
+                            error!("Error transcribing audio: {error:?}");
                         }
-                        println!("Moving {path:?} to {to:?}");
+                        debug!("Moving {path:?} to {to:?}");
                         if let Err(error) = std::fs::create_dir_all(&output) {
-                            eprintln!("Error creating directory: {error:?}");
+                            error!("Error creating directory: {error:?}");
                         }
                         if let Err(error) = rename(path, to) {
-                            eprintln!("Error renaming: {error:?}");
+                            error!("Error renaming: {error:?}");
                             continue;
                         }
                     }
                 }
             }
-            Err(error) => println!("Error: {error:?}"),
+            Err(error) => error!("Error: {error:?}"),
         }
     }
 
@@ -68,8 +79,13 @@ fn listen(input: PathBuf, output: PathBuf, output_format: &str) -> notify::Resul
 }
 
 fn reformat_filename(filename: &Path, output_format: &str) -> String {
-    if let Some(date_time) = get_file_date(filename) {
-        format!("{}.mp3", date_time.format(output_format))
+    if let Some(date_time) = get_earliest_file_date(filename) {
+        let extension = filename
+            .extension()
+            .and_then(|s| s.to_str())
+            .map(|ext| format!(".{}", ext))
+            .unwrap_or_default();
+        format!("{}{}", date_time.format(output_format), extension)
     } else {
         filename.to_str().unwrap().to_string()
     }
@@ -77,7 +93,7 @@ fn reformat_filename(filename: &Path, output_format: &str) -> String {
 
 fn transcribe_audio_file(input: &Path, output: &Path) -> Result<(), String> {
     // TODO: Actual call to audio processing library
-    println!("Transcribing file from {:?} to {:?}", input, output);
+    debug!("Transcribing file from {:?} to {:?}", input, output);
     Ok(())
 }
 
@@ -107,7 +123,7 @@ fn is_supported_audio_format(path: &Path) -> bool {
         .unwrap_or(false)
 }
 
-fn get_file_date(path: &Path) -> Option<DateTime<Utc>> {
+fn get_earliest_file_date(path: &Path) -> Option<DateTime<Local>> {
     let meta = match metadata(path) {
         Ok(meta) => meta,
         Err(_) => {
@@ -115,9 +131,9 @@ fn get_file_date(path: &Path) -> Option<DateTime<Utc>> {
         }
     };
 
-    let creation_date = meta.created().or_else(|_| meta.modified());
-    match creation_date {
-        Ok(system_time) => Some(DateTime::<Utc>::from(system_time)),
-        Err(_) => None,
-    }
+    let created_time = meta.created().unwrap_or(SystemTime::UNIX_EPOCH);
+    let modified_time = meta.modified().unwrap_or(SystemTime::UNIX_EPOCH);
+    let earliest_time = if created_time < modified_time { created_time } else { modified_time };
+
+    Some(DateTime::<Utc>::from(earliest_time).with_timezone(&Local))
 }
