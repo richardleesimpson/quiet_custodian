5363db8f957d513c7c36fcc194f4c55932dd64e3
richardleesimpson <github.elag9@passmail.net>
2024-05-07 22:17:08 -0500

    Add symphonia for audio decoding

--- src/audio_utils.rs
+++ src/audio_utils.rs
@@ -1,5 +1,15 @@
-use std::{ ffi::OsStr, path::Path };
-use crate::{ debug, error::AppError };
+use std::{ ffi::OsStr, fs::File, path::Path };
+use symphonia::core::{
+    audio::SampleBuffer,
+    codecs::DecoderOptions,
+    errors::Error,
+    formats::FormatOptions,
+    io::MediaSourceStream,
+    meta::MetadataOptions,
+    probe::Hint,
+};
+
+use crate::{ debug, error::AppError, info };
 
 pub enum AudioFormat {
     MP3,
@@ -27,8 +37,140 @@ pub fn is_supported_audio_format(path: &Path) -> bool {
         .unwrap_or(false)
 }
 
+fn decode_audio_file(path: &Path) -> Result<Vec<i16>, AppError> {
+    // Create a media source. Note that the MediaSource trait is automatically implemented for File,
+    // among other types.
+    let file = Box::new(File::open(path).unwrap());
+
+    // Create the media source stream using the boxed media source from above.
+    let mss = MediaSourceStream::new(file, Default::default());
+
+    // Create a hint to help the format registry guess what format reader is appropriate. In this
+    // example we'll leave it empty.
+    let hint = Hint::new();
+
+    // Use the default options when reading and decoding.
+    let format_opts: FormatOptions = Default::default();
+    let metadata_opts: MetadataOptions = Default::default();
+    let decoder_opts: DecoderOptions = Default::default();
+
+    // Probe the media source stream for a format.
+    let probed = symphonia::default
+        ::get_probe()
+        .format(&hint, mss, &format_opts, &metadata_opts)
+        .unwrap();
+
+    // Get the format reader yielded by the probe operation.
+    let mut format = probed.format;
+
+    // Get the default track.
+    let track = format.default_track().unwrap();
+
+    // Create a decoder for the track.
+    let mut decoder = symphonia::default
+        ::get_codecs()
+        .make(&track.codec_params, &decoder_opts)
+        .unwrap();
+
+    // Store the track identifier, we'll use it to filter packets.
+    let track_id = track.id;
+
+    let mut sample_count = 0;
+    let mut sample_buf = None;
+    let mut samples: Vec<i16> = Vec::new();
+
+    // The decode loop.
+    loop {
+        // Get the next packet from the media format.
+        let packet = match format.next_packet() {
+            Ok(packet) => packet,
+            Err(Error::ResetRequired) => {
+                // The track list has been changed. Re-examine it and create a new set of decoders,
+                // then restart the decode loop. This is an advanced feature and it is not
+                // unreasonable to consider this "the end." As of v0.5.0, the only usage of this is
+                // for chained OGG physical streams.
+                unimplemented!();
+            }
+            Err(symphonia::core::errors::Error::IoError(error)) if
+                error.kind() == std::io::ErrorKind::UnexpectedEof
+            => {
+                // audio source ended
+                info!("Decoded {} total samples", sample_count);
+                return Ok(samples);
+            }
+            Err(err) => {
+                // A unrecoverable error occurred, halt decoding.
+                panic!("{}", err);
+            }
+        };
+
+        // Consume any new metadata that has been read since the last packet.
+        while !format.metadata().is_latest() {
+            // Pop the old head of the metadata queue.
+            format.metadata().pop();
+
+            // Consume the new metadata at the head of the metadata queue.
+        }
+
+        // If the packet does not belong to the selected track, skip over it.
+        if packet.track_id() != track_id {
+            continue;
+        }
+
+        // Decode the packet into audio samples.
+        match decoder.decode(&packet) {
+            Ok(audio_buf) => {
+                // The decoded audio samples may now be accessed via the audio buffer if per-channel
+                // slices of samples in their native decoded format is desired. Use-cases where
+                // the samples need to be accessed in an interleaved order or converted into
+                // another sample format, or a byte buffer is required, are covered by copying the
+                // audio buffer into a sample buffer or raw sample buffer, respectively. In the
+                // example below, we will copy the audio buffer into a sample buffer in an
+                // interleaved order while also converting to a f32 sample format.
+
+                // If this is the *first* decoded packet, create a sample buffer matching the
+                // decoded audio buffer format.
+                if sample_buf.is_none() {
+                    // Get the audio buffer specification.
+                    let spec = *audio_buf.spec();
+
+                    // Get the capacity of the decoded buffer. Note: This is capacity, not length!
+                    let duration = audio_buf.capacity() as u64;
+
+                    // Create the i16 sample buffer.
+                    sample_buf = Some(SampleBuffer::<i16>::new(duration, spec));
+                }
+
+                // Copy the decoded audio buffer into the sample buffer in an interleaved format.
+                if let Some(buf) = &mut sample_buf {
+                    buf.copy_interleaved_ref(audio_buf);
+
+                    // The samples may now be access via the `samples()` function.
+                    let buf_samples = buf.samples();
+                    sample_count += buf_samples.len();
+                    samples.extend_from_slice(buf_samples);
+                    debug!("Decoded {} samples", sample_count);
+                }
+            }
+            Err(Error::IoError(_)) => {
+                // The packet failed to decode due to an IO error, skip the packet.
+                continue;
+            }
+            Err(Error::DecodeError(_)) => {
+                // The packet failed to decode due to invalid data, skip the packet.
+                continue;
+            }
+            Err(err) => {
+                // An unrecoverable error occurred, halt decoding.
+                panic!("{}", err);
+            }
+        }
+    }
+}
+
 pub fn transcribe_audio(input: &Path, output: &Path) -> Result<(), AppError> {
-    // TODO: Actual call to audio processing library
     debug!("Transcribing file from {:?} to {:?}", input, output);
+    decode_audio_file(input)?;
+    // TODO: Actual call to audio processing library
     Ok(())
 }
